5e1deb0bb3854e5fb6dc5d141a48faa4a09ef204
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-03-14 12:43:00 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,45 @@
+import React, {useState} from "react";
 import video from "../data/video.js";
+import Video from "./Video.js";
+import Comments from "./Comments.js";
 
 function App() {
-  console.log("Here's your data:", video);
+  //console.log("Here's your data:", video);
+
+  const [upVotes, setUpVotes] = useState(video.upvotes);
+  const [downVotes, setDownVotes] = useState(video.downvotes);
+
+  const [comments, setComments] = useState([...video.comments]);
+  const [showComments, setShowComments] = useState(true);
+
+  function handleUpVote(){
+    setUpVotes((upVotes) => ++upVotes);
+  }
+
+  function handleDownVote(){
+    setDownVotes((downVotes) => ++downVotes);
+  }
+
+  function handleComments(){
+    setShowComments((showComments) => !showComments);
+  }
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
+      <Video 
+      videoURL={video.embedUrl} 
+      title={video.title} 
+      views={video.views} 
+      createdAt={video.createdAt}
+      upVotes={upVotes}
+      downVotes={downVotes}
+      handleUpVote={handleUpVote}
+      handleDownVote={handleDownVote}
+      handleComments={handleComments}
+      commentBool={showComments}
+      />
+      <Comments comments={comments}
+      commentBool={showComments}
       />
     </div>
   );
